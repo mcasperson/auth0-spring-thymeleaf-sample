f28928df99ef2fdde742175f52b34e7a45574489
Matthew Casperson <matthewcasperson@gmail.com>
2021-07-23 17:30:25 +1000

    Use auth config from application.yml

--- src/main/java/com/matthewcasperson/auth0demo/configuration/AuthSecurityConfig.java
+++ src/main/java/com/matthewcasperson/auth0demo/configuration/AuthSecurityConfig.java
@@ -1,103 +1,35 @@
 package com.matthewcasperson.auth0demo.configuration;
 
 import com.matthewcasperson.auth0demo.oauth.CustomAuthorizationRequestResolver;
-import com.matthewcasperson.auth0demo.oauth.CustomRequestEntityConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
-import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/oauth_login").permitAll()
+                .antMatchers("/oauth2/**").permitAll()
                 .anyRequest().authenticated()
             .and()
             .oauth2Login()
                 .authorizationEndpoint()
                 .authorizationRequestResolver(
                         new CustomAuthorizationRequestResolver(
-                                clientRegistrationRepository(), "/oauth2/authorize-client"))
-                .baseUri("/oauth2/authorize-client")
-                .authorizationRequestRepository(authorizationRequestRepository());
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientService authorizedClientService() {
-
-        return new InMemoryOAuth2AuthorizedClientService(
-                clientRegistrationRepository());
-    }
-
-    @Bean
-    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
-        return new HttpSessionOAuth2AuthorizationRequestRepository();
-    }
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient(){
-        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient =
-                new DefaultAuthorizationCodeTokenResponseClient();
-        accessTokenResponseClient.setRequestEntityConverter(new CustomRequestEntityConverter(System.getenv().get("AUDIENCE")));
-
-        OAuth2AccessTokenResponseHttpMessageConverter tokenResponseHttpMessageConverter =
-                new OAuth2AccessTokenResponseHttpMessageConverter();
-
-        RestTemplate restTemplate = new RestTemplate(Arrays.asList(
-                new FormHttpMessageConverter(), tokenResponseHttpMessageConverter));
-        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
-
-        accessTokenResponseClient.setRestOperations(restTemplate);
-        return accessTokenResponseClient;
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("auth0")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId}")
-                .clientId(System.getenv().get("CLIENT_ID"))
-                .clientSecret(System.getenv().get("CLIENT_SECRET"))
-                .issuerUri("https://matthewcasperson.au.auth0.com/")
-                .authorizationUri("https://matthewcasperson.au.auth0.com/authorize")
-                .tokenUri("https://matthewcasperson.au.auth0.com/oauth/token")
-                .jwkSetUri("https://matthewcasperson.au.auth0.com/.well-known/jwks.json")
-                .scope("openid", "profile", "email")
-                .clientName("auth0")
-                .build();
-
-        return new InMemoryClientRegistrationRepository(clientRegistration);
+                                clientRegistrationRepository, "/oauth2/authorization"));
     }
 }

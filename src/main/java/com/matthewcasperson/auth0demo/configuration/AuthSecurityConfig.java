041a3243c979ef14233197d703c8cc13b986d45d
Matthew Casperson <matthewcasperson@gmail.com>
2021-07-23 17:39:14 +1000

    Cleaned up imports

--- src/main/java/com/matthewcasperson/auth0demo/configuration/AuthSecurityConfig.java
+++ src/main/java/com/matthewcasperson/auth0demo/configuration/AuthSecurityConfig.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
 
 @EnableWebSecurity
 @Configuration

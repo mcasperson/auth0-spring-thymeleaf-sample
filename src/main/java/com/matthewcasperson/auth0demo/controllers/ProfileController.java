e0869e6c78ae9e4e754ead2331acb8eaa5a97678
Matthew Casperson <matthewcasperson@gmail.com>
2021-08-13 08:36:35 +1000

    Update ProfileController.java

--- src/main/java/com/matthewcasperson/auth0demo/controllers/ProfileController.java
+++ src/main/java/com/matthewcasperson/auth0demo/controllers/ProfileController.java
@@ -36,7 +36,7 @@ public class ProfileController {
         try {
             return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(attributes);
         } catch (JsonProcessingException e) {
-            return "{\"message\":\"" + e.toString() + "\"};
+            return "{\"message\":\"" + e + "\"}";
         }
     }
 }
